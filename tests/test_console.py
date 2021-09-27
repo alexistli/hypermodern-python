8884b5dee0cd1c1d73c15fa7fb8f55de6729ea87
alexistli <alexis.torelli.treanton@gmail.com>
2021-09-27 21:07:12 +0200

    fix poetry and add pytest-mock

---
+++ tests/test_console.py
@@ -0,0 +1,48 @@
+import click.testing
+import pytest
+
+from hypermodern_python import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+def test_main_succeeds(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+
