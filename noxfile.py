3fbc4ed4e99341345029d3b9080232230ba32621
alexistli <alexis.torelli.treanton@gmail.com>
2021-09-28 02:51:41 +0200

    Code formatting with Black

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,21 @@
 import nox
 
-
 locations = "src", "tests", "noxfile.py"
+nox.options.sessions = "lint", "tests"
+
+
+@nox.session(python=["3.9", "3.8"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.9", "3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black")
+    session.run("flake8", *args)
 
 
 @nox.session(python=["3.9", "3.8"])
@@ -9,10 +23,3 @@ def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
-
-
-@nox.session(python=["3.9", "3.8"])
-def lint(session):
-    args = session.posargs or locations
-    session.install("flake8")
-    session.run("flake8", *args)
