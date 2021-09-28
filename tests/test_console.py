9892fd47d91073ca4c1469b2600297894e7af243
alexistli <alexis.torelli.treanton@gmail.com>
2021-09-28 02:29:17 +0200

    Example CLI: Selecting the Wikipedia language edition

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,13 +11,8 @@ def runner():
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hypermodern_python.wikipedia.random_page")
 
 
 def test_main_succeeds(runner, mock_requests_get):
@@ -53,3 +48,6 @@ def test_main_prints_message_on_request_error(runner, mock_requests_get):
     assert "Error" in result.output
 
 
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
