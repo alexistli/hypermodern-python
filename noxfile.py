956b9e37a8271c3e2ea024f698ed8fce608bb0c1
alexistli <alexis.torelli.treanton@gmail.com>
2021-09-29 10:01:02 +0200

    Runtime type checking with Typeguard

--- noxfile.py
+++ noxfile.py
@@ -75,6 +75,14 @@ def pytype(session: Session) -> None:
 
 
 @nox.session(python=["3.9", "3.8"])
+def typeguard(session):
+    args = session.posargs or ["-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
+    session.run("pytest", f"--typeguard-packages={package}", *args)
+
+
+@nox.session(python=["3.9", "3.8"])
 def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
