1e44e89e57a95c38d65d8e3e4eedb7161033497a
alexistli <alexis.torelli.treanton@gmail.com>
2021-09-30 19:19:06 +0200

    Adding type annotations to the test suite

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -6,13 +6,13 @@ import pytest
 from hypermodern_python import wikipedia
 
 
-def test_random_page_uses_given_language(mock_requests_get: Mock):
+def test_random_page_uses_given_language(mock_requests_get: Mock) -> None:
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
 
 
-def test_random_page_returns_page(mock_requests_get: Mock):
+def test_random_page_returns_page(mock_requests_get: Mock) -> None:
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
 
